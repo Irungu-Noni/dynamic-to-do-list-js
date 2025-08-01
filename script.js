c882a7cd15cf135e9daf08646dff30d8a1ac7d6a
Irungu-Noni <kamsycally@gmail.com>
2025-08-01 16:10:33 +0300

    Updated Script file

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ document.addEventListener('DOMContentLoaded', function()
 {
     const addButton = document.getElementById('add-task-btn');
     const taskInput = document.getElementById('task-input');
-    const itemList = document.getElementById('task-List');
+    const taskList = document.getElementById('task-List');
 
     // Function to add a new task
     function addTask() {
